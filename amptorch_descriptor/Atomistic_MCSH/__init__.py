83e211ac39d293ad7fdaee23543a8ce56afa4824
ray38 <xlei38@gatech.edu>
2020-08-19 17:11:36 -0400

    fixed bug

--- amptorch_descriptor/Atomistic_MCSH/__init__.py
+++ amptorch_descriptor/Atomistic_MCSH/__init__.py
@@ -186,15 +186,16 @@ class AtomisticMCSH(AMPTorchDescriptorBase):
             print("ERROR: descriptor not IMPLEMENTED!!")
         # print(errno)
         fp = np.array(x)
-        
+
         fp_prime = np.array(dx)
-        super_threshold_indices = np.abs(fp_prime) > 1e-10
-        fp_prime[super_threshold_indices] = 0.0
+        # super_threshold_indices = np.abs(fp_prime) > 1e-10
+        # fp_prime[super_threshold_indices] = 0.0
 
         scipy_sparse_fp_prime = sparse.coo_matrix(fp_prime)
         # print(fp)
         # print(fp.shape)
-        # print(scipy_sparse_fp_prime.data)
+        print(scipy_sparse_fp_prime.data)
+        print(np.min(np.abs(scipy_sparse_fp_prime.data)))
         print("density: {}% \n\n----------------------".format(100*len(scipy_sparse_fp_prime.data) / (fp_prime.shape[0] * fp_prime.shape[1])))
 
         return size_info, fp, scipy_sparse_fp_prime.data, scipy_sparse_fp_prime.row, scipy_sparse_fp_prime.col, np.array(fp_prime.shape)
